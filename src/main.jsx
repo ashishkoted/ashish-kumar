58a405e6d14dd01abed03d6cd2a1880da0267b3e
Ashish Kumar <dnashu890@gmail.com>
2026-06-19 16:05:46 +0530

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+
 import './index.css'
 import { SoundProvider } from "./context/SoundContext"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <SoundProvider>
-    <App />
-  </SoundProvider>
+  <StrictMode>
+    <SoundProvider>
+      <App />
+      <Analytics />
+    </SoundProvider>
+  </StrictMode>
 )
